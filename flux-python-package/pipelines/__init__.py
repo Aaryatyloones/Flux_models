aba12b1e8d5472bc73843d3642fa2c5ad89bdcce
Aaryatyloones <aarya@tyloones.com>
2025-01-28 23:39:06 -0800

    first commit

---
+++ flux-python-package/pipelines/__init__.py
@@ -0,0 +1,5 @@
+from .schnell_text2img import SchnellText2ImgPipeline
+from .schnell_img2img import SchnellImg2ImgPipeline
+from .dev_text2img import DevText2ImgPipeline
+from .dev_img2img import DevImg2ImgPipeline
+from .dev_upscale import DevUpscalePipeline
